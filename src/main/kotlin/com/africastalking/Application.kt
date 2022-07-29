6a07d93e73c572ccb2ad63e6ba1c9357106b7bfa
muchbeer <gman2them@gmail.com>
2022-07-29 09:54:11 +0300

    change the embeddedServer

--- src/main/kotlin/com/africastalking/Application.kt
+++ src/main/kotlin/com/africastalking/Application.kt
@@ -6,7 +6,8 @@ import com.africastalking.route.*
 import io.ktor.server.application.*
 
 fun main() {
-    embeddedServer(Netty, port = 8088, host = "192.168.31.23") {
+  //  embeddedServer(Netty, port = 8088, host = "192.168.31.23") {
+    embeddedServer(Netty,port = System.getenv("PORT").toInt()) {
         log.info("Ktor server enter at services")
         configureSerialization()
 
