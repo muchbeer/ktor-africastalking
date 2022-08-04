6f3f542592387721386b97e0d9b7c03b858682b0
muchbeer <gman2them@gmail.com>
2022-08-04 17:16:38 +0300

    remove auth first

--- src/main/kotlin/com/africastalking/route/Serialization.kt
+++ src/main/kotlin/com/africastalking/route/Serialization.kt
@@ -1,15 +1,10 @@
 package com.africastalking.route
 
-import com.africastalking.model.DataState
-import com.africastalking.security.JwtConfig
-import com.africastalking.security.SessionIdPrincipalForMenu
 import io.ktor.serialization.jackson.*
 import com.fasterxml.jackson.databind.*
 import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.application.*
-import io.ktor.server.auth.*
-import io.ktor.server.auth.jwt.*
-import io.ktor.server.response.*
+
 
 fun Application.configureSerialization() {
     install(ContentNegotiation) {
